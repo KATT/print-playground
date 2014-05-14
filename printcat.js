a5a9b35c6a2af03478f7ab79912afd9d69216e65
Alexander Johansson <ajax@ustwo.co.uk>
2014-05-14 11:38:13 +0100

    move stuff around

--- printcat.js
+++ printcat.js
@@ -1,8 +1,6 @@
 var print = require('./lib/print');
 var fs = require('fs');
 
-var DEVICE_ADDRESS = '8ca6bb1717f0a6fa';
-
-var cat = fs.readFileSync('cat.txt', 'utf8');
+var cat = fs.readFileSync('./fixtures/cat.txt', 'utf8');
 
 print(cat);
