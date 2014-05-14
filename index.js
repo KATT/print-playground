4378d7f0bad437b5496e59924343f30b6abdf51b
Alexander Johansson <ajax@ustwo.co.uk>
2014-05-14 11:33:10 +0100

    printing interface!

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');
+var print = require('./lib/print');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser())
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.post('/print', function(req, res) {
+	var str = req.body.string;
+	if (str) {
+		print(str);
+	}
+	res.writeHead(301, {"Location": "/"});
+    res.end();
 });
 
 app.listen(app.get('port'), function() {
