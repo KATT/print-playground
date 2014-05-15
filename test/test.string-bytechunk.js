ca1b36b6113addd9fc8d5ef65a021c153e47b9ad
Alexander Johansson <ajax@ustwo.co.uk>
2014-05-15 14:56:17 +0100

    split the string up in chunks by the max length allowed by berg cloud

---
+++ test/test.string-bytechunk.js
@@ -0,0 +1,18 @@
+var stringBytechunk = require('../lib/string-bytechunk');
+
+var assert = require("assert");
+
+describe('string-bytechunk', function(){
+	it('should return one chunk if less bytes than specified', function(){
+		var str = 'abc';
+		assert.deepEqual(stringBytechunk(str, 4), [str]);
+	})
+	it('should return one chunk if equal bytes as specified', function(){
+		var str = 'abc';
+		assert.deepEqual(stringBytechunk(str, 3), [str]);
+	})
+	it('should chunk up in multiple chunks', function(){
+		assert.deepEqual(stringBytechunk('abcdef', 3), ['abc', 'def']);
+		assert.deepEqual(stringBytechunk('abcdefg', 3), ['abc', 'def', 'g']);
+	})
+})
